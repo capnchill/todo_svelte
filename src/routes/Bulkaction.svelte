964e90bcfcae83782a17148cfa90ec5cccc5390f
capnchill <subodhchilwal123@gmail.com>
2024-04-09 12:33:56 +0530

    Added remove complete functionality

--- src/routes/Bulkaction.svelte
+++ src/routes/Bulkaction.svelte
@@ -1,12 +1,28 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	let dispatch = createEventDispatcher();
+
+	function clickCheckAll() {
+		dispatch('CheckAll');
+	}
+
+	function clickRemoveCompleted() {
+		dispatch('removeCompleted');
+	}
+</script>
+
 <div class="mt-4">
 	<div class="flex gap-2">
 		<button
 			class="mt-10 flex-1 bg-black py-1 text-lg font-extralight text-white hover:underline focus:outline-none sm:mt-0"
+			on:click={clickCheckAll}
 		>
 			Check All
 		</button>
 		<button
 			class="mt-10 flex-1 bg-black py-1 text-lg font-extralight text-white hover:underline focus:outline-none sm:mt-0"
+			on:click={clickRemoveCompleted}
 		>
 			Remove Completed
 		</button>
--- src/routes/Todos.svelte
+++ src/routes/Todos.svelte
@@ -25,6 +25,22 @@
 		todos.splice(id, 1);
 		todos = todos;
 	}
+
+	function toggleCheck() {
+		if (todos.filter((todo) => todo.completed === false).length !== 0) {
+			todos.forEach((todo) => (todo.completed = true));
+			todos = todos;
+		} else if (todos.filter((todo) => todo.completed === true).length !== 0) {
+			todos.forEach((todo) => (todo.completed = false));
+			todos = todos;
+		}
+	}
+
+	function removeCompleted() {
+		let active = todos.filter((todo) => todo.completed === false);
+		todos = active;
+		console.log(active);
+	}
 </script>
 
 <div class="mx-auto mb-4">
@@ -48,5 +64,5 @@
 
 	<div class="border-[1px] border-slate-400"></div>
 
-	<Bulkaction />
+	<Bulkaction on:CheckAll={toggleCheck} on:removeCompleted={removeCompleted} />
 {/if}
